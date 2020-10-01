fdc828a7d393f1d2c5e58f02e08a4e59dc19d4e3
rahulshastryd <48375702+rahulshastryd@users.noreply.github.com>
2020-10-01 12:47:59 +0530

    Update SimpleCalc.py

--- SimpleCalc.py
+++ SimpleCalc.py
@@ -1,65 +1,40 @@
-# Program make a simple calculator that can add, subtract, multiply and divide using functions
-
-# This function adds two numbers 
-def add(x, y):
-   return x + y
-
-# This function subtracts two numbers 
-def subtract(x, y):
-   return x - y
-
-# This function multiplies two numbers
-def multiply(x, y):
-   return x * y
-
-# This function divides two numbers
-def divide(x, y):
-   return x / y
-
-# This function calcuates x to the power of y
-def power(x, y):
-  result = 1
-  counter = 0
-  while counter < y:
-    result = result * x
-    counter += 1
-  return result  
+#Simple Calculator program using class
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-print("5.Power Of")
+class cal():
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+    def add(self):
+        return self.a + self.b
+    def sub(self):
+        return self.a - self.b
+    def multiply(self):
+        return self.a * self.b
+    def divide(self):
+        return self.a / self.b
 
-isRunning = True
-
-while isRunning:
-  choice = input("Enter choice(1/2/3/4/5) or 0 for exit:")
-
-  if choice == '1' or choice == '2' or choice == '3' or choice == '4' or choice == '5':
-    num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number: "))
-    isRunning = False
-  elif choice == '0':
-    print("Exiting program..")
-    exit()
-  else:
-    print("Invalid input! Please, try again.")
-    
-
-  if choice == '1':
-    print(num1,"+",num2,"=", add(num1,num2))
-
-  elif choice == '2':
-    print(num1,"-",num2,"=", subtract(num1,num2))
-
-  elif choice == '3':
-    print(num1,"*",num2,"=", multiply(num1,num2))
-
-  elif choice == '4':
-    print(num1,"/",num2,"=", divide(num1,num2))
-
-  elif choice == '5':
-    print(num1,"to the power of ",num2,"=", power(num1,num2))
+a = int(input('Enter First number : '))
+b = int(input('Enter Second number : '))        
+obj=cal(a,b)
+while True:
+    def menu():
+        x = ('1. Add \n2. Sub \n3. Multiply \n4. Divide') 
+        print(x)
+    menu()
+    choice = int(input('Please select one of the following : ')) 
+    if choice == 1:
+        print("Result: ",obj.add())
+    elif choice == 2:
+        print("Result: ",obj.sub())
+    elif choice == 3:
+        print("Result: ",obj.multiply())    
+    elif choice == 4:
+        print("Result: ",obj.divide())
+    elif choice == 0:
+        print('Again try one of the following')
+        break
+    else:
+        print('Invalid option') 
+        break       
+print()
